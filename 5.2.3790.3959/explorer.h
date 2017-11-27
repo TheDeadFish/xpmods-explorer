6bf132d8558f2971ee5e1bea7e3a0928a0012e66
TheDeadFish <michaelstamper1@gmail.com>
2017-11-27 04:48:52 +0000

    1. added forgotten file 5.2.3790.3959/explorer.h

---
+++ 5.2.3790.3959/explorer.h
@@ -0,0 +1,22 @@
+#ifndef _EXPLORER_H_
+#define _EXPLORER_H_
+
+// 
+#define Stobject_Base 0x748F0000
+#define VolNotify_NopB 0x748F51D6
+#define VolNotify_NopE 0x748F51E4
+#define VolNotify_Cp1 0x748F51E7
+#define Volume_Timer 0x748F51F6
+
+#define DrawText_Offset (0x7748DE64-0x77420000)
+
+
+
+
+
+
+
+
+
+
+#endif
